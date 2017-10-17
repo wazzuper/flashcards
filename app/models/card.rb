6c30dd543b6a5cc999264cfaacf01880308a707c
artem <clifredd@gmail.com>
2017-10-17 14:22:11 +0500

    Fixed Card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,18 +5,17 @@ class Card < ApplicationRecord
 
   before_create :review_date
 
-  private
-
   def original_text=(value)
-    write_attribute(:original_text, value) if value
+    write_attribute(:original_text, value.to_s.strip.capitalize) if value
   end
 
   def translated_text=(value)
-    write_attribute(:translated_text, value.to_s.strip.capitalize!) if value
+    write_attribute(:translated_text, value.to_s.strip.capitalize) if value
   end
+
+  private
 
   def original_and_translated_texts_are_not_the_same
     errors.add(:translated_text, 'Не может быть таким же как и перевод') if translated_text == original_text
   end
-
 end
