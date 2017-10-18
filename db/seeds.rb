a257d5807ae82a4bb6f75aa73fe16fcae32686ef
artem <clifredd@gmail.com>
2017-10-18 18:26:13 +0500

    Script fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,14 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-# Взял страницу со словами из примера
 page = Nokogiri::HTML(open('http://1000mostcommonwords.com/1000-most-common-russian-words/'))
 table_link = page.css('div#page div#content div#primary div#main article#post-162 table tbody tr td')
 
-# Создал пустой массив
-original_and_translated_texts = []
 num1 = 4
 num2 = 5
 
-# num1 идет раньше num2, поэтому в сравнении идет он
 while num1 < table_link.length
-  original_and_translated_texts << [table_link[num1].text, table_link[num2].text]
-  num1 +=3
-  num2 +=3
-end
-
-original_and_translated_texts.each do |original, translated|
-  Card.create(original_text: original, translated_text: translated, review_date: Date.today + 3.days)
+  Card.create(original_text: table_link[num1].text, translated_text: table_link[num2].text, review_date: Date.today + 3.days)
+  num1 += 3
+  num2 += 3
 end
