534bbdb82987facf519a28af05ea31f5ef6d3951
artem <clifredd@gmail.com>
2017-10-18 18:34:46 +0500

    Configure HoundCi#2

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,8 @@ num1 = 4
 num2 = 5
 
 while num1 < table_link.length
-  Card.create(original_text: table_link[num1].text, translated_text: table_link[num2].text, review_date: Date.today + 3.days)
+  Card.create(original_text: table_link[num1].text,translated_text: table_link[num2].text,
+              review_date: Date.today + 3.days)
   num1 += 3
   num2 += 3
 end
