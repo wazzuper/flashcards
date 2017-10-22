c2272f263f5c91cc794b2c3e2633f690dbc92dba
artem <clifredd@gmail.com>
2017-10-22 11:55:51 +0500

    Script fixed #3

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,12 @@ require 'nokogiri'
 require 'open-uri'
 
 page = Nokogiri::HTML(open('http://1000mostcommonwords.com/1000-most-common-russian-words/'))
-table_link = page.css('div#page div#content div#primary div#main article#post-162 table tbody tr')
+cells = page.css('div#page div#content div#primary div#main article#post-162 table tbody tr')
 
-table_link.each do |line|
-  next if line.element_children[0].text == 'Number'
-  Card.create(original_text: line.last_element_child.previous_element.text,
-              translated_text: line.last_element_child.text, review_date: Date.today + 3.days)
+cells.delete(cells.first)
+
+cells.each do |line|
+  Card.create(original_text: line.css('td')[1].text,
+              translated_text: line.css('td')[2].text,
+              review_date: Date.today + 3.days)
 end
